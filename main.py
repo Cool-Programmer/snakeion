30656496ddbbff1ff83f8dceb7856bfd4422e6e7
= <margaryan.mher.28@gmail.com>
2016-11-16 12:35:09 +0400

    I. Bare bones set.

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+pygame.init()  # Initialize pygame
+gameDisplay = pygame.display.set_mode((800, 600))  # Game window
+gameTitle = pygame.display.set_caption('Snakeion')  # Set title
+
+pygame.display.update()  # Update flip
+pygame.quit()  # Uninitialize pygame
+quit()  # Quit
